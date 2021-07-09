92f3ccc72db936b982286e60f56ff2ad5d6d15ff
daniellima338 <daniellima338@gmail.com>
2021-07-09 20:17:54 +0000

    added more structure and comments to the Javascript. Having troubles getting the answers to display. WIP

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,43 +1,37 @@
 // Used to get the data
 $.ajax({
-    url: "https://opentdb.com/api.php?amount=10&type=multiple",
+    url: "https://opentdb.com/api.php?amount=20&category=9&type=multiple",
     type: 'GET',
-    dataType: 'json', // added data type
-        success: function(response) {
-           setupQuiz(response.results);
-        }
-    });
+    success: function(response) {
+        setupQuiz(response.results);
+    }
+});
 
-// Taken from stack Overflow. Function that randomize the order of the questions.
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
+// Used to set up a new question everytime a answer is clicked
 function setupQuiz(data){
 
     var current_question_id = 0
-    setupNewQuestion(data[current_question_id])
+    setupAnswers(data[current_question_id])
 
     $("button").click(function() {
-            current_question_id++
-            setupNewQuestion(data[current_question_id])
+        current_question_id++;
+        setupAnswers(data[current_question_id])
     });
-    
-    console.log(data)            
-    }
+    console.log(data);       
+}
 
-function setupNewQuestion(current){
-    $("#question").text(current.question)
+// Used to recycle the correct answer everytime. CANT GET THE ANSWERS TO DISPLAY
+function setupAnswers(results){
+    $("#question").text(results.question)
 
-    answers = [current.correct_answer]
-                .concat(current.incorrect_answers)
-                    .sort((a, b) => 0.5 - Math.random());
-         var i = 1;
-         answers.forEach(function (answer) {
-             $("#answer_" + i).text(answer)  
-             i++
-         })
-          
-     }
+     answers = [results.correct_answer]
+                     .concat(results.incorrect_answers)
+                     .sort((a, b) => 0.5 - Math.random());
+     
+     var i = 0;
+     answers.forEach(function (answer) {
+         $("#answer_" + i).text(answer)  
+         i++
+     })
+      
+ }
